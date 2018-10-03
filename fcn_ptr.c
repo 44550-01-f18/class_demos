1a7cd8d628b69d87a022703acd9c1d7ab8939825
Nathan Eloe <powerofazure@gmail.com>
2018-10-03 13:08:21 -0500

    Add another example of a function pointer

--- fcn_ptr.c
+++ fcn_ptr.c
@@ -4,10 +4,17 @@ void do_a_thing(void (*my_func) ());
 void foo();
 void bar();
 
+int square(int x);
+int cube(int x);
+
+void do_another_thing(int (*my_func) (int a));
+
 int main(int argc, char* argv[])
 {
 	do_a_thing(foo);
 	do_a_thing(bar);
+	do_another_thing(square);
+	do_another_thing(cube);
 	return 0;
 }
 
@@ -24,4 +31,17 @@ void bar()
 void do_a_thing(void (*my_func) ())
 {
 	my_func();
+}
+
+void do_another_thing(int (*my_func) (int a))
+{
+	printf("%d\n", my_func(4));
+}
+int square(int x)
+{
+	return x*x;
+}
+int cube(int x)
+{
+	return x*x*x;
 }
